db1dd733e06d4d54c76dd7f564adc76f2bd6f0d7
san <stcr96@gmail.com>
2024-07-15 09:42:36 +0900

    ✅ Solved 5-5

---
+++ L5/5-5.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).unwrap();
+    let n: usize = n.trim().parse().unwrap();
+    
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).unwrap();
+    let line: Vec<char> = line.trim().chars().collect();
+    
+    let mut sum: u32 = 0;
+    for i in 0..n {
+        sum += line[i].to_digit(10).unwrap();
+    }
+    
+    println!("{}", sum);
+}
